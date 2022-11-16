d55e490c1b5a52b25d1f6d19b80733bda6ea56d0
Aravind Roy <aravindroy1709@gmail.com>
2022-11-16 12:36:54 +0530

    userSlice modified and store created

--- src/Redux/Features/userSlice.js
+++ src/Redux/Features/userSlice.js
@@ -1,15 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-
+    isSignedIn:false,
+    userData:null,
+    searchInput:"tech",
+    blogData:null
 }
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {}
+  reducers: {
+    SETISSIGNEDIN:(state , action)=>{
+        state.isSignedIn = action.payload
+    },
+    SETUSERDATA:(state,action)=>{
+        state.userData = action.payload
+    },
+    SETSEARCHINPUT:(state , action)=>{
+        state.searchInput = action.payload
+    },
+    SETBLOGDATA:(state,action)=>{
+        state.blogData = action.payload
+    }
+  }
 });
 
-export const {} = userSlice.actions
+export const {SETBLOGDATA , SETSEARCHINPUT ,SETSIGNEDIN ,SETUSERDATA} = userSlice.actions
 
+export const selectIsSignedIn = (state)=> state.user.isSignedIn
+export const selectUserData = (state)=>state.user.userData
+export const selectSearchInput = (state)=>state.user.searchInput
+export const selectBlogData = (state)=>state.user.blogData
 export default userSlice.reducer
